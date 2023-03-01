f6d13e6124129524e055f45619b54449a3b52070
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-03-01 23:51:14 +0000

    Removed dropzone dependencies from smart form module

---
+++ projects/azlabs/ngx-smart-form/src/lib/angular/components/ngx-smart-file-input/types.ts
@@ -0,0 +1,16 @@
+/**
+ * File Input constraints type declaration
+ * 
+ * @internal
+ */
+export type InputConstraints = {
+    maxFiles?: number;
+    maxFilesize?: number;
+}
+
+/**
+ * Set state parameter type declaration
+ * 
+ * @internal
+ */
+export type SetStateParam<T> = Partial<T> | ((state: T) => T);
