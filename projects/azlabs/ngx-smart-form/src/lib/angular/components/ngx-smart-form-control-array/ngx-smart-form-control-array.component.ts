de9d228a70f3967ad683acab804d6ed1052edef2
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-03-02 19:15:04 +0000

    Fixed issue with form array

--- projects/azlabs/ngx-smart-form/src/lib/angular/components/ngx-smart-form-control-array/ngx-smart-form-control-array.component.ts
+++ projects/azlabs/ngx-smart-form/src/lib/angular/components/ngx-smart-form-control-array/ngx-smart-form-control-array.component.ts
@@ -66,8 +66,6 @@ export class NgxSmartFormControlArrayComponent implements OnInit, OnDestroy {
   }
   //#endregion Component inputs definitions
 
-  //@internal
-  private abstractControl!: FormControl;
   @ViewChild('container', { read: ViewContainerRef, static: true })
   viewContainerRef!: ViewContainerRef;
 
@@ -88,9 +86,6 @@ export class NgxSmartFormControlArrayComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.abstractControl = this.builder.control(
-      this.inputConfig
-    ) as FormControl;
     if (this.formArray.getRawValue().length === 0) {
       this.addNewComponent(this.componentRefCount);
     } else {
@@ -117,7 +112,9 @@ export class NgxSmartFormControlArrayComponent implements OnInit, OnDestroy {
 
   // tslint:disable-next-line: typedef
   addNewComponent(index: number) {
-    const control = cloneAbstractControl(this.abstractControl) as FormControl;
+    const control = cloneAbstractControl(
+      this.builder.control(this.inputConfig)
+    ) as FormControl;
     this.addComponent(control, index);
     this.formArray.push(control);
   }
